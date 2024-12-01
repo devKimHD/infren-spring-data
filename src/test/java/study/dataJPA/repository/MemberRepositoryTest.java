a9d1a55205e30ecf3791c44db3fc431afa9ea3aa
KIMHYUNGDONG <khd30425@gmail.com>
2024-12-01 16:21:07 +0900

    실무에선 안쓰지만 알아는 보는 queryExample

--- src/test/java/study/dataJPA/repository/MemberRepositoryTest.java
+++ src/test/java/study/dataJPA/repository/MemberRepositoryTest.java
@@ -6,10 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -310,6 +307,30 @@ class MemberRepositoryTest {
         Specification<Member> spec = MemberSpec.userName("m1").and(MemberSpec.teamName("teamA"));
         List<Member> result = memberRepository.findAll(spec);
         Assertions.assertThat(result.size()).isEqualTo(1);
+    }
+    @Test
+    public void queryByExample() throws Exception
+    {
+        //given
+        Team team = new Team("teamA");
+        em.persist(team);
+        Member m1 = new Member("m1", 0, team);
+        Member m2 = new Member("m2", 0, team);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+        //when
+        //probe
+        Member member = new Member("m1");
+        Team teamA = new Team("teamA");
+        member.changeTeam(teamA);
+        ExampleMatcher matcher = ExampleMatcher.matching().withIgnorePaths("age");
+        Example<Member> example = Example.of(member,matcher);
+        List<Member> result = memberRepository.findAll(example);
+        Assertions.assertThat(result.get(0).getUsername()).isEqualTo("m1");
+        //then
     }
 
 }
